6345de71451b5ffe658bc05d16a999dd3e337b84
Pratish Joshi <joshipratish997@gmail.com>
2022-03-10 23:06:10 +0545

    Lab 1-4 Completed

---
+++ Lab 4/Homework/app/src/main/java/com/example/constraintlayout/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+    public static final String EXTRA_MESSAGE = "message";
+    private Button Hello;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        Hello = findViewById(R.id.button_hello);
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent i = new Intent(this, SecondActivity.class);
+        String passedMessage = mShowCount.getText().toString();
+        i.putExtra(EXTRA_MESSAGE, passedMessage);
+        startActivity(i);
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
